de4b28d83a51249b4f191b38c0e062ed71554b54
Hoang Do <nkhdo@smu.edu.sg>
2016-10-27 17:40:28 +0800

    send packet OK

--- intropkt.c
+++ intropkt.c
@@ -6,17 +6,42 @@
 #include <linux/netdevice.h>
 #include <net/udp.h>
 
+int t = 100; /* Time interval in ms */
+int p; /* Probability threshold */
+
 char* ifname = "eth1";
-struct net_device *odev;
+__u32 src_ip_addr = 0x0102a8c0;
+__u32 dst_ip_addr = 0x0103a8c0;
+__u64 src_mac_addr = 0x180373b2bb5b;
+__u64 dst_mac_addr = 0xf0bf97697d74;
+__u16 src_port = 123;
+__u16 dst_port = 1234;
 
-int intropkt_find_net_device(void);
+static struct net_device *intropkt_find_net_device(char *ifname) {
+	struct net_device *dev = first_net_device(&init_net);
+	while (dev) {
+	    printk(KERN_INFO "[%s]\n", dev->name);
+	    if (!strcmp(ifname, dev->name))
+	    	break;
+	    dev = next_net_device(dev);
+	}
 
-static struct sk_buff *intropkt_alloc_skb(struct net_device *dev) {
+	if (dev) {
+		printk(KERN_INFO "found the dev %s\n", dev->name);
+	}
+	else {
+		printk(KERN_INFO "cannot find the dev %s\n", ifname);
+	}
+
+	return dev;
+}
+
+static struct sk_buff *intropkt_alloc_skb(struct net_device *dev, int pktlen) {
 	unsigned int extralen = LL_RESERVED_SPACE(dev);
 	struct sk_buff *skb = NULL;
 	unsigned int size;
 
-	size = 64 + 64 + extralen;
+	size = pktlen + 64 + extralen;
 
 	skb = __netdev_alloc_skb(dev, size, GFP_NOWAIT);
 
@@ -26,102 +51,121 @@ static struct sk_buff *intropkt_alloc_skb(struct net_device *dev) {
 	return skb;
 }
 
-// static struct sk_buff *intropkt_fill_skb(struct net_device *dev) {
-// 	struct sk_buff *skb = NULL;
-// 	__u8 *eth;
-// 	struct udphdr *udph;
-// 	int datalen, iplen;
-// 	struct iphdr *iph;
-// 	__be16 protocol = htons(ETH_P_IP);
-// 	__be32 *mpls;
-// 	__be16 *vlan_tci = NULL;                 /* Encapsulates priority and VLAN ID */
-// 	__be16 *vlan_encapsulated_proto = NULL;  /* packet type ID field (or len) for VLAN tag */
-// 	__be16 *svlan_tci = NULL;                /* Encapsulates priority and SVLAN ID */
-// 	__be16 *svlan_encapsulated_proto = NULL; /* packet type ID field (or len) for SVLAN tag */
-// 	u16 queue_map;
+static void intropkt_fill_skb_data(struct sk_buff *skb, char *data, int datalen) {
+	char *pktdata;
+	pktdata = (char *) skb_put(skb, datalen);
 
+	strcpy(pktdata, data);
+}
 
-// 	skb = pktgen_alloc_skb(dev);
-// 	if (!skb) {
-// 		printk(KERN_INFO "No memory\n");
-// 		return NULL;
-// 	}
+static struct sk_buff *intropkt_create_skb(struct net_device *dev, char* data) {
+	struct sk_buff *skb = NULL;
+	__u8 *eth;
+	struct udphdr *udph;
+	int pktlen, datalen, iplen;
+	struct iphdr *iph;
+	__be16 protocol = htons(ETH_P_IP);
+	__be32 *mpls;
 
-// 	prefetchw(skb->data);
-// 	skb_reserve(skb, 16);
+	datalen = strlen(data) + 1; //??
+	pktlen = datalen + 14 + 20 +8;
 
-// 	/*  Reserve for ethernet and IP header  */
-// 	eth = (__u8 *) skb_push(skb, 14);
-// 	mpls = (__be32 *)skb_put(skb, pkt_dev->nr_labels*sizeof(__u32));
-
-// 	skb_reset_mac_header(skb);
-// 	skb_set_network_header(skb, skb->len);
-// 	iph = (struct iphdr *) skb_put(skb, sizeof(struct iphdr));
-
-// 	skb_set_transport_header(skb, skb->len);
-// 	udph = (struct udphdr *) skb_put(skb, sizeof(struct udphdr));
-// 	skb_set_queue_mapping(skb, queue_map);
-// 	skb->priority = pkt_dev->skb_priority;
-
-// 	memcpy(eth, pkt_dev->hh, 12);
-// 	*(__be16 *) & eth[12] = protocol;
-
-// 	/* Eth + IPh + UDPh + mpls */
-// 	datalen = 64 - 14 - 20 - 8;
-
-// 	udph->source = htons(80);
-// 	udph->dest = htons(80);
-// 	udph->len = htons(datalen + 8);	/* DATA + udphdr */
-// 	udph->check = 0;
-
-// 	iph->ihl = 5;
-// 	iph->version = 4;
-// 	iph->ttl = 32;
-// 	iph->tos = 0;
-// 	iph->protocol = IPPROTO_UDP;	/* UDP */
-// 	iph->saddr = pkt_dev->cur_saddr;
-// 	iph->daddr = pkt_dev->cur_daddr;
-// 	iph->id = htons(pkt_dev->ip_id);
-
-// 	iph->frag_off = 0;
-// 	iplen = 20 + 8 + datalen;
-// 	iph->tot_len = htons(iplen);
-// 	ip_send_check(iph);
-// 	skb->protocol = protocol;
-// 	skb->dev = dev;
-// 	skb->pkt_type = PACKET_HOST;
-
-// 	//intropkt_finalize_skb(pkt_dev, skb, datalen);
-
-// 	skb->ip_summed = CHECKSUM_NONE;
-	
-
-// 	return skb;
-// }
-
-int intropkt_find_net_device(void) {
-	odev = first_net_device(&init_net);
-	while (odev) {
-	    printk(KERN_INFO "found [%s]\n", odev->name);
-	    if (!strcmp(ifname, odev->name))
-	    	break;
-	    odev = next_net_device(odev);
+	skb = intropkt_alloc_skb(dev, pktlen);
+	if (!skb) {
+		printk(KERN_INFO "No memory\n");
+		return NULL;
 	}
 
-	if (odev) {
-		printk(KERN_INFO "found the dev %s\n", odev->name);
-		return 0;
-	}
-	else {
-		printk(KERN_INFO "cannot find the dev %s\n", ifname);
-		return -1;
-	}
+	prefetchw(skb->data);
+	skb_reserve(skb, 16);
+
+	/*  Reserve for ethernet and IP header  */
+	eth = (__u8 *) skb_push(skb, 14);
+	mpls = (__be32 *)skb_put(skb, 0);
+
+	skb_reset_mac_header(skb);
+	skb_set_network_header(skb, skb->len);
+	iph = (struct iphdr *) skb_put(skb, sizeof(struct iphdr));
+
+	skb_set_transport_header(skb, skb->len);
+	udph = (struct udphdr *) skb_put(skb, sizeof(struct udphdr));
+	skb->priority = 0;
+
+	__u8 hh[14];
+
+	hh[0] = dst_mac_addr >> 40;
+	hh[1] = dst_mac_addr >> 32;
+	hh[2] = dst_mac_addr >> 24;
+	hh[3] = dst_mac_addr >> 16;
+	hh[4] = dst_mac_addr >> 8;
+	hh[5] = dst_mac_addr;
+	hh[6] = src_mac_addr >> 40;
+	hh[7] = src_mac_addr >> 32;
+	hh[8] = src_mac_addr >> 24;
+	hh[9] = src_mac_addr >> 16;
+	hh[10] = src_mac_addr >> 8;
+	hh[11] = src_mac_addr;
+	hh[12] = 0x00;
+	hh[13] = 0x00;
+
+	memcpy(eth, hh, 12);
+
+	*(__be16 *) & eth[12] = protocol;
+
+	udph->source = htons(src_port);  /* source port  */
+	udph->dest = htons(dst_port); /* dest port */
+	udph->len = htons(datalen + 8);	/* DATA + udphdr */
+	udph->check = 0;
+
+	iph->ihl = 5;
+	iph->version = 4;
+	iph->ttl = 32;
+	iph->tos = 0;
+	iph->protocol = IPPROTO_UDP;	/* UDP */
+	iph->saddr = src_ip_addr; /* source ip */
+	iph->daddr = dst_ip_addr; /* dest ip */
+	iph->id = htons(0);
+
+	iph->frag_off = 0;
+	iplen = 20 + 8 + datalen;
+	iph->tot_len = htons(iplen);
+	ip_send_check(iph);
+	skb->protocol = protocol;
+	skb->dev = dev;
+	skb->pkt_type = PACKET_HOST;
+
+	skb->ip_summed = CHECKSUM_NONE;
+
+	intropkt_fill_skb_data(skb, data, datalen);
+
+	return skb;
 }
 
 int init_module(void)
 {
 	printk(KERN_INFO "insmod intropkt\n");
-	intropkt_find_net_device();
+
+	struct net_device* dev = intropkt_find_net_device(ifname);
+
+	struct sk_buff *skb = NULL;
+
+	char *testdata = "this is a test data. hahahahahah";
+
+	skb = intropkt_create_skb(dev, testdata);
+
+	int ret = dev_queue_xmit(skb);
+	switch (ret) {
+	case NET_XMIT_SUCCESS:
+		printk(KERN_INFO "packet is sent\n");
+		break;
+	case NET_XMIT_DROP:
+	case NET_XMIT_CN:
+	case NETDEV_TX_BUSY:
+	default:
+		printk(KERN_INFO "packet is not sent\n");
+		break;
+	}
+
 	return 0;
 }
 
@@ -132,5 +176,5 @@ void cleanup_module()
 
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Hoang Do <nkhdo@smu.edu.sg>");
+MODULE_AUTHOR("Hoang");
 MODULE_DESCRIPTION("A kernel module for sending introspection packets");
