a9d4b1adccbbdaf86ed408c29abe19f4afa40782
Hoang Do <nkhdo@smu.edu.sg>
2016-10-29 20:08:25 +0800

    remove uneccessary files

--- cleanup.c
+++
@@ -1,7 +0,0 @@
-#include "intropkt.h"
-
-void cleanup_module()
-{
-	printk(KERN_INFO "rmmod intropkt\n");
-}
-
--- intropkt.h
+++
@@ -1,6 +0,0 @@
-#include <linux/kernel.h>
-#include <linux/module.h>
-
-uint64_t get_tdh(void);
-void set_tdh(uint64_t addr);
-
